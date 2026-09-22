bae8dc67c7963519cf740ec05c3aaeadee4a1353
schubertsean-ui <256894134+schubertsean-ui@users.noreply.github.com>
2026-09-21 20:23:36 -0500

    Add first crate of photographed jackets

---
+++ photo-crate.js
@@ -0,0 +1,4 @@
+window.PHOTO_CRATE = [
+  {"id":"diz-n-bird","title":"Diz 'n' Bird: The Beginning","artist":"Dizzy Gillespie & Charlie Parker","label":"Roost / Roulette","catalog":"SK-106","genre":["Jazz"],"speed":"33","images":{"front":"https://lh3.googleusercontent.com/d/13tFHMjnjiI63kmlQJT20ADV6Zq_MHgER=w800","back":"https://lh3.googleusercontent.com/d/1LZD1TLyvUUCmyVO4u4f5PjkuQIjrVW6N=w800"}},
+  {"id":"bowmar-dances-i","title":"Dances, Part I","artist":"Bowmar Orchestral Library","label":"Bowmar","catalog":"BOL 55","year":1965,"genre":["Educational"],"speed":"33","images":{"front":"https://lh3.googleusercontent.com/d/1s0example","back":""}}
+];
